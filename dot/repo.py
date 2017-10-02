d36f7e2fb36f49d0624542817ba742e4c39825bb
MatToufoutu <mattoufootu@gmail.com>
2017-10-02 08:36:13 +0200

    create DotRepository class structure

--- dot/repo.py
+++ dot/repo.py
@@ -2,4 +2,23 @@
 
 
 class DotRepository:
-    pass
+    def __init__(self, cfg):
+        self.cfg = cfg
+
+    def cmd_init(self, args):
+        raise NotImplementedError
+
+    def cmd_gpgid(self, args):
+        raise NotImplementedError
+
+    def cmd_add(self, args):
+        raise NotImplementedError
+
+    def cmd_rm(self, args):
+        raise NotImplementedError
+
+    def cmd_sync(self, args):
+        raise NotImplementedError
+
+    def cmd_publish(self, args):
+        raise NotImplementedError
