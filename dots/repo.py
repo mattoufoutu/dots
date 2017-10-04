76b89a8e462526b36ef2b93ca88e2a8b3a7a7e4b
MatToufoutu <mattoufootu@gmail.com>
2017-10-04 05:53:28 +0200

    more explicit "not implemented" errors

--- dots/repo.py
+++ dots/repo.py
@@ -6,19 +6,19 @@ class DotRepository:
         self.cfg = cfg
 
     def cmd_init(self, args):
-        raise NotImplementedError
+        raise NotImplementedError("the 'init' is not implemented yet")
 
     def cmd_gpgid(self, args):
-        raise NotImplementedError
+        raise NotImplementedError("the 'gpgid' command is not implemented yet")
 
     def cmd_add(self, args):
-        raise NotImplementedError
+        raise NotImplementedError("the 'add' command is not implemented yet")
 
     def cmd_rm(self, args):
-        raise NotImplementedError
+        raise NotImplementedError("the 'rm' command is not implemented yet")
 
     def cmd_sync(self, args):
-        raise NotImplementedError
+        raise NotImplementedError("the 'sync' command is not implemented yet")
 
     def cmd_publish(self, args):
-        raise NotImplementedError
+        raise NotImplementedError("the 'publish' command is not implemented yet")
