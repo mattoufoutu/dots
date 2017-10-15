33f46d2b9a44db55adf33f363ad8a52e24f05a36
MatToufoutu <mattoufootu@gmail.com>
2017-10-15 19:57:23 +0200

    add TODO

--- dots/cli.py
+++ dots/cli.py
@@ -21,7 +21,6 @@ def parse_args():
         help='custom repository path',
         metavar='DIR'
     )
-
     parser.add_argument(
         '-V', '--version',
         help='display program version and exit',
@@ -36,6 +35,8 @@ def parse_args():
 
     parser_init = subparsers.add_parser('init', help='initialize dots repository')
     parser_init.set_defaults(func='init')
+
+    # TODO: implement 'list' command
 
     parser_add = subparsers.add_parser('add', help='add file to the repository')
     parser_add.set_defaults(func='add')
